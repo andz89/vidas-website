49166c974810fa4b9d1d874715e02dbe8523fa1d
andz <andzrivero89@gmail.com>
2021-07-23 22:16:29 -0700

    add effects

---
+++ dist/script/script.js
@@ -0,0 +1,23 @@
+
+
+let contact = document.querySelector(".contact")
+let main = document.querySelector("main")
+let nav_home = document.querySelector(".nav_home")
+let nav_contact = document.querySelector(".nav_contact")
+
+nav_contact.addEventListener('click', function(){
+    contact.style.display = "block";
+    nav_contact.style.color = "#d15f20"
+    nav_home.style.color = "#fff"
+
+    main.style.display = "none"
+
+})
+nav_home.addEventListener('click', function(){
+    contact.style.display = "none";
+    nav_contact.style.color = "#fff"
+    nav_home.style.color = "#d15f20"
+
+    main.style.display = "block"
+
+})
