8a001aa992127d5a4f62a89843219a6c76608e03
andz <andzrivero89@gmail.com>
2021-07-31 11:05:27 -0700

    add loading css

--- dist/script/nav.js
+++ dist/script/nav.js
@@ -5,26 +5,24 @@ let nav_ul_span = document.querySelector("ul span")
 let ul_background = document.querySelector('.ul_background')
 menu.addEventListener("click", function(e){
  
-ul_background.style.display = "block"
+  ul_background.style.transform = "translateX(0%)";
 
 })
 
 nav_ul_span.addEventListener("click", function(){
  
-ul_background.style.display = "none"
+ul_background.style.transform = "translateX(100%)";
 
 
 })
-// window.addEventListener("load",()=>{
 
-  
-// })
   
 window.addEventListener('click', outsideClick);
 // Close If Outside Click
 function outsideClick(e) {
 if (e.target == ul_background) {
-ul_background.style.display = 'none';
+  ul_background.style.transform = "translateX(100%)";
+
 
 }
 }
@@ -41,20 +39,21 @@ ul.addEventListener("click", function(e){
 function clickLinks(e) {
 
   if(e.classList == "link"){
-    document.querySelector(".container").classList.add("spinner-1");
+    document.querySelector("body").classList.add("spinner-1");
     document.querySelector("main").style.display = "none";
+    document.querySelector(".container").style.display = "none";
     a.forEach(function(r){
       r.style.color = "white"
     e.style.color = "#d15f20"
     })
   }
 }
+document.querySelector("body").classList.add("spinner-1");
+
+window.addEventListener('DOMContentLoaded', () => {
+  document.querySelector("body").classList.remove("spinner-1");
+});
 
 
-
-
-
-// document.querySelector(".container").classList.remove("spinner-1");
-// document.querySelector("main").style.display = "block";
 
 
