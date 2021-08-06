ee6d635e586400beac2919c1c2a66ea6a8c079bf
andz <andzrivero89@gmail.com>
2021-08-06 09:46:49 -0700

    rm files

--- dist/script/data_birthday.js
+++ dist/script/data_birthday.js
@@ -4,22 +4,65 @@ export const data_birthday = [
   {
     id : "b1",
     name:"Mac Queen Cars Invitation",
-    price: "20.00 Php",
-    description: "10 pieces Minimun order<br> Printed in glossy photo paper <br>We will add a picture of the celebrant on the invitation.<br>Colored envelope included.<br> Layout or the design is free.<br>One sided print only.<br>Invitation and envelope sizes approximately 4x5 inches landscape <br><br> <strong>How to Order</strong> <br> Call or message us to reserve your order or  if you have questions. See the contact details below.<br> Please send us the following details: <br> 1. Name of the celebrant <br> 2. High quality picture of the celebrant <br>3. Age<br> 4. Date and time of the event <br>5. Venue of the party <br> <small style='color:red'>This is made to order. Please allow us at least 1 week lead time.</small> ",
+    price: "25.00 Php",
+    description: `
+    10 pcs Minimun order <br>
+    Printed in glossy photo paper <br>
+    We will add a picture of the celebrant on the invitation.<br>
+    Colored envelope included. <br> 
+    Layout or the design is free. <br>
+    One sided print only. <br>
+    Invitation and envelope sizes approximately 4x5 inches landscape <br>
+    <br> 
+
+    <strong style="font-size: 1.2rem;">How to Order</strong> <br> 
+
+    Call or message us to reserve your order or  if you have questions. See the contacts below.<br> 
+    Please send us the following details: <br> 
+    1. Name of the celebrant <br> 
+    2. High quality picture of the celebrant <br>
+    3. Age <br>
+    4. Date and time of the event <br>
+    5. Venue of the party <br>
+    6. Your additional details.<br> 
+    <small style='color:red'>This is made to order. Please allow us at least 1 week lead time.</small><br>
+    `,
     source:  "img/cars.jpg",
-    link:'',
+    link:'https://drive.google.com/drive/folders/1YrgVVnbHfB3yQd0JO-o-msweUO_A5uSb?usp=sharing',
   
 
 },
 {
   id : "b2",
-    name:"Paw Patrol Invitation",
-    size: "5x7 in, 4x5 in, 3x4 in",
-    price: "10.00 Php",
-    description: " Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque maxime iure perspiciatis, ut vero molestias culpa consequuntur quidem sed explicabo excepturi natus ipsa aliquam quibusdam voluptate dolores tempore consequatur minima!Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque maxime iure perspiciatis, ut vero molestias culpa consequuntur quidem sed explicabo excepturi natus ipsa aliquam quibusdam voluptate dolores tempore consequatur!",
-    source:  "img/1000px.jpg",
-    link:''
+  name:"Paw Patrol Invitation",
+  price: "25.00 Php",
+  description: `
+  10 pcs Minimun order <br>
+  Printed in glossy photo paper <br>
+  We will add a picture of the celebrant on the invitation.<br>
+  Colored envelope included. <br> 
+  Layout or the design is free. <br>
+  One sided print only. <br>
+  Invitation and envelope sizes approximately 4x5 inches landscape <br>
+  <br> 
+  
+    <strong style="font-size: 1.2rem;">How to Order</strong> <br> 
 
+  Call or message us to reserve your order or  if you have questions. See the contacts below.<br> 
+  Please send us the following details: <br> 
+  1. Name of the celebrant <br> 
+  2. High quality picture of the celebrant <br>
+  3. Age <br>
+  4. Date and time of the event <br>
+  5. Venue of the party <br>
+  6. Your additional details.<br> 
+  <small style='color:red'>
+  This is made to order. Please allow us at least 1 week lead time.
+  </small>
+
+  `,
+  source:  "img/paw-patrol.jpg",
+  link:'https://drive.google.com/drive/folders/1diI2JPw6MPOoL4g7r1Bih5Y-fZVHlrVz?usp=sharing',
 
 }
 
--- dist/script/data_wedding.js
+++ dist/script/data_wedding.js
@@ -3,8 +3,21 @@ export const data_wedding = [
 
     {
       id : "w1",
-      name:"Wedding Souvenir Picture With Stand",
-      description: " Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque maxime iure perspiciatis, ut vero molestias culpa consequuntur quidem sed explicabo excepturi natus ipsa aliquam quibusdam voluptate dolores tempore consequatur minima!Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque maxime iure perspiciatis, ut vero molestias culpa consequuntur quidem sed explicabo excepturi natus ipsa aliquam quibusdam voluptate dolores tempore consequatur.",
+      name:"Wedding Souvenir Picture With Display Stand",
+      description: `
+    25 pcs Minimun order <br>
+    Printed in glossy photo paper <br>
+    Layout or the design is free. <br>
+    Size approximately 8 inches height and 4 inches width <br>
+    <br> 
+    <strong style="font-size: 1.2rem;">How to Order</strong> <br> 
+    Call or message us to reserve your order or  if you have questions. See the contacts below.<br> 
+    Please send us the following details: <br> 
+    1. High quality picture and Name of the Couple <br> 
+    2. Date<br>
+    3. Your additional details.<br> 
+    <small style='color:red'>This is made to order. Please allow us at least 1 week lead time.</small><br>
+      `,
       price: "75.00 Php",
       source:  "img/w1.jpg",
       link:"https://drive.google.com/drive/folders/1fYLbBGAuYbj9CJRuQsFplhNKcyNRCBvV?usp=sharing"
@@ -15,7 +28,20 @@ export const data_wedding = [
   {
     id : "w2",
       name:"Old Rose Ref Magnet Wedding Souvenir",
-      description: "Ito po ay maganda tulad ng may ari nito",
+      description: `
+      20 pcs Minimun order <br>
+      Printed in glossy photo paper <br>
+      Layout or the design is free. <br>
+      Size approximately 8 inches height and 4 inches width <br>
+      <br> 
+      <strong style="font-size: 1.2rem;">How to Order</strong> <br> 
+      Call or message us to reserve your order or  if you have questions. See the contacts below.<br> 
+      Please send us the following details: <br> 
+      1. High quality picture and Name of the Couple <br> 
+      2. Date<br>
+      3. Your additional details.<br> 
+      <small style='color:red'>This is made to order. Please allow us at least 1 week lead time.</small><br>
+      `,
       price: "20.00 Php",
       source:  "img/wedding.jpg",
       link:"https://drive.google.com/drive/folders/1fYLbBGAuYbj9CJRuQsFplhNKcyNRCBvV?usp=sharing"
