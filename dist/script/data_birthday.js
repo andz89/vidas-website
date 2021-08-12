8fa6e03eba73ea9110b81decbbaebe261fc8f5a4
andz <andzrivero89@gmail.com>
2021-08-12 12:01:40 -0700

    commit

--- dist/script/data_birthday.js
+++ dist/script/data_birthday.js
@@ -5,6 +5,7 @@ export const data_birthday = [
     id : "b1",
     name:"Mac Queen Cars Invitation",
     price: "20.00 Php",
+
     display:true,
     description: `
     10 pcs Minimun order <br>
@@ -15,7 +16,7 @@ export const data_birthday = [
     One sided print only. <br>
     Invitation and envelope sizes approximately 4x5 inches landscape <br>
 
-    <p style="font-style:italic;color:#f35a5a; font-size: .9rem">  If you want only the invitation card, you can purchase it for 10 pesos each. 20 pcs Minimum order.</p>
+ 
     <br>
 
     <strong style="font-size: 1rem;">How to Order</strong> <br> 
@@ -29,7 +30,7 @@ export const data_birthday = [
     4. Date and time of the event <br>
     5. Venue of the party <br>
     6. Your additional details.<br> 
-  
+    <p style="font-style:italic;color:#f35a5a; font-size: .8rem">  If you want only the invitation card, you can purchase it for 10 pesos each. 20 pcs Minimum order.</p>
     `,
     source:  "img/cars.jpg",
     link:'https://drive.google.com/drive/folders/1YrgVVnbHfB3yQd0JO-o-msweUO_A5uSb?usp=sharing',
@@ -50,7 +51,7 @@ export const data_birthday = [
     One sided print only. <br>
     Invitation and envelope sizes approximately 4x5 inches landscape <br>
 
-    <p style="font-style:italic;color:#f35a5a; font-size: .9rem">  If you want only the invitation card, you can purchase it for 10 pesos each. 20 pcs Minimum order.</p>
+
     <br>
   
     <strong style="font-size: 1rem;">How to Order</strong> <br> 
@@ -64,6 +65,7 @@ export const data_birthday = [
     4. Date and time of the event <br>
     5. Venue of the party <br>
     6. Your additional details.
+    <p style="font-style:italic;color:#f35a5a; font-size: .9rem">  If you want only the invitation card, you can purchase it for 10 pesos each. 20 pcs Minimum order.</p>
   `,
   source:  'img/paw-patrol.jpg',
   link:'https://drive.google.com/drive/folders/1diI2JPw6MPOoL4g7r1Bih5Y-fZVHlrVz?usp=sharing ',
@@ -83,7 +85,7 @@ export const data_birthday = [
     One sided print only. <br>
     Invitation and envelope sizes approximately 4x5 inches landscape <br>
 
-    <p style="font-style:italic;color:#f35a5a; font-size: .9rem">  If you want only the invitation card, you can purchase it for 10 pesos each. 20 pcs Minimum order.</p>
+
     <br>
   
 
@@ -97,6 +99,7 @@ export const data_birthday = [
     4. Date and time of the event <br>
     5. Venue of the party <br>
     6. Your additional details.
+    <p style="font-style:italic;color:#f35a5a; font-size: .9rem">  If you want only the invitation card, you can purchase it for 10 pesos each. 20 pcs Minimum order.</p>
   `,
   source:  'img/pj-mask.jpg',
   link:'https://drive.google.com/drive/folders/1KRd-v7SHOKGTQjjyflyVsMhNT3F9Llzj?usp=sharing ',
--- dist/script/script.js
+++ dist/script/script.js
@@ -71,152 +71,255 @@ function clickLinks(e) {
 
 import {data_birthday} from "./data_birthday.js"
 import {data_wedding} from "./data_wedding.js"
+
 let modal = document.createElement('div')
 modal.className += "modal" 
 
 let span = document.createElement('span')
 span.className += "close" 
 span.innerHTML =`
- <span class="close">← Back</span> 
+← Back
 `
-
+let next_btn = document.createElement('small')
+next_btn.className += "next" 
+next_btn.innerHTML =`
+Next
+`
+let prev_btn = document.createElement('small')
+prev_btn.className += "prev" 
+prev_btn.innerHTML =`
+Prev
+`
 
 document.body.appendChild(modal)
 
 
 
 
+let div;
+let  index = 0
+let new_array;
 
 
-
-// let modal = document.querySelector(".modal")
 function getId(e){
-    
+
     if( e.classList == "identifier"){
         
          let id = e.nextElementSibling.getAttribute('id')
-    
-      //clone an array
-      //remove the target data
-      //go the orignal array get the data of target array
-      //make a new array and add the target data and add also the clone array
-
-      
-       
-
-  
-
-
-         let id_type;
+ 
         if(id.startsWith("b")){
-          let clone;
-          clone = data_birthday.filter(function(e){
-         return e.id != id
-         })
-        
-         let target_data = data_birthday.filter(function(e){
-           return e.id == id
-         })
 
-        let new_array = target_data.concat(clone);
+        // let clone;
+        //   clone = data_birthday.filter(function(e){
+        //  return e.id != id
+        //  })
+        //  let target_data = data_birthday.filter(function(e){
+        //    return e.id == id
+        //  })
 
-          id_type = new_array
+        index = data_birthday.findIndex(e =>{
+        return  e.id == id
+        })
+        new_array= data_birthday;
         }
+
         if(id.startsWith("w")){
-          let clone;
-          clone = data_wedding.filter(function(e){
-         return e.id != id
-         })
-        
-         let target_data = data_wedding.filter(function(e){
-           return e.id == id
-         })
+        index = data_wedding.findIndex(e =>{
+        return  e.id == id
+        })
+        new_array= data_wedding;
 
-        let new_array = target_data.concat(clone);
-
-          id_type = new_array
-  
         }
-
-       id_type.forEach(function(data){
-        let div = document.createElement('div')
-        div.className += "modal-content" 
-
-        div.innerHTML = `
-          <div class="image-div">
-     
-         <a href="${data.link}" target="blank">
-        <img src="${data.source}"  alt="...">
-        </a>
-        <div class="view-image"><strong>•</strong> <p>Click the image to view more photos</p></div>
-        </div>
-
-        <div id="${data.id}" class=details>
-     
-        
-          <h5>${data.name}</h5>
-          <h2>${data.price}</h2>
-          <p class="header_description">Product Details:</p>
-          <p class="description">${data.description}</p>
-
-          <div class="modal-contact">
-          <h4> Contact us for your oders and querries:</h4>
-          <div class="modal-flex">
-                    <div class="modal-call">
-                        
-                        <div class="img_and_numbers">  <img src="img/call.svg"   alt=""> <p>827-3520 / 0955-846-4400</p></div>
-                    </div>
-  
-                    <div class="modal-text">
-                        <div class="img_and_link"> <img  src="img/facebook5.svg"  alt=""> 
-                        <small>Click the link below to message us on facebook </small>
-                        <a href="https://www.facebook.com/gwillevent">https://www.facebook.com/gwillevent</a></div>
-                    </div>
-            </div>
-        </div>
-        </div>
-  
-        `
-        document.querySelector("body").style.overflow = 'hidden';
-        modal.style.display = 'block';
-        modal.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-        modal.appendChild(span)
-        modal.appendChild(div)
-     
-        
-        let closeBtn = document.querySelector(".close")
-        closeBtn.addEventListener('click', closeModal)
-        window.addEventListener('click', outsideClick);
-
-        // Close
-        function closeModal() {
-        modal.style.display = 'none';
-        document.querySelector("body").style.overflow = 'auto';
-        modal.innerHTML = '';
-
-      
-        }
-
-      //  Outside Click close
-      function outsideClick(e) {
-      if (e.target == modal) {
-      modal.style.display = 'none';
-      document.querySelector("body").style.overflow = 'auto';
-      modal.innerHTML = '';
-
-   
-      }
-      }
-      })
-
+    
+      display_data(new_array[index])
       
     }
 }
+
+
 //modal e target
-document.querySelector("main").addEventListener("click",function(e){
-    
-    getId(e.target)
+document.querySelector("main").addEventListener("click",function(e){ 
+  getId(e.target)
 })
 
+modal.addEventListener("click", function(e){
+  next(e.target)
+
+})
+
+function next(e){
+  if(e.classList == "next"){
+
+    index++
+
+    if(index== new_array.length){
+      //return to first slide
+        index=0;
+     }
+     div.remove()
+     display_data(new_array[index])
+  }
+  if(e.classList == "prev"){
+    if(index == 0){
+    index = new_array.length-1;
+    }else{
+    index--;
+    }
+ div.remove()
+ display_data(new_array[index])
+}
+
+}
+
+
+
+
+function display_data(data){
+div = document.createElement('div')
+  div.className +=  "modal-container"
+  div.innerHTML = `
+  <div class="modal-content">
+    <div class="image-div">
+
+   <a href="${data.link}" target="blank">
+  <img src="${data.source}"  alt="...">
+  </a>
+  <div class="view-image"><strong>•</strong> <p>Click the image to view more photos</p></div>
+  </div>
+
+  <div id="${data.id}" class=details>
+
+  
+    <h5>${data.name}</h5>
+    <h2>${data.price}</h2>
+    <p class="header_description">Product Details:</p>
+    <p class="description">${data.description}</p>
+
+    <div class="modal-contact">
+    <h4> Contact us for your oders and querries:</h4>
+    <div class="modal-flex">
+              <div class="modal-call">
+                  
+                  <div class="img_and_numbers">  <img src="img/call.svg"   alt=""> <p>827-3520 / 0955-846-4400</p></div>
+              </div>
+
+              <div class="modal-text">
+                  <div class="img_and_link"> <img  src="img/facebook5.svg"  alt=""> 
+                  <small>Click the link below to message us on facebook </small>
+                  <a href="https://www.facebook.com/gwillevent">https://www.facebook.com/gwillevent</a></div>
+              </div>
+      </div>
+  </div>
+  </div>
+  </div>
+
+  `
+
+  document.querySelector("body").style.overflow = 'hidden';
+  modal.style.display = 'block';
+  modal.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  
+  modal.appendChild(next_btn)
+  modal.appendChild(prev_btn)  
+  modal.appendChild(div)
+  modal.appendChild(span)
+
+
+  let closeBtn = document.querySelector(".close")
+  closeBtn.addEventListener('click', closeModal)
+  window.addEventListener('click', outsideClick);
+
+  // Close
+  function closeModal() {
+  modal.style.display = 'none';
+  document.querySelector("body").style.overflow = 'auto';
+  div.remove()
+  index = 0
+  }
+
+//  Outside Click close
+function outsideClick(e) {
+let modal_container = document.querySelector(".modal-container")
+if (e.target == modal_container) {
+modal.style.display = 'none';
+document.querySelector("body").style.overflow = 'auto';
+div.remove()
+index = 0
+}
+}
+
+
+
+}
+
+
+let isDragging = false,
+  startPos = 0,
+  currentTranslate = 0,
+  prevTranslate = 0
+ 
+
+
+  modal.addEventListener('dragstart', (e) => e.preventDefault())
+
+  // Touch events
+  modal.addEventListener('touchstart', touchStart())
+  modal.addEventListener('touchend', touchEnd)
+  modal.addEventListener('touchmove', touchMove)
+
+  function touchStart(){
+   
+    return function(event){
+     
+      startPos = getPositionX(event)
+      console.log(startPos)
+  
+      isDragging = true;
+    }
+  }
+
+  function touchEnd(){
+    isDragging = false
+    
+   
+
+    const movedBy = currentTranslate - prevTranslate
+
+    if (movedBy < -100 ) {
+      index++
+
+    if(index== new_array.length){
+      //return to first slide
+        index = 0;
+     }
+     div.remove()
+     display_data(new_array[index])
+      
+  
+    }
+
+    if (movedBy > 100){
+
+      if(index == 0){
+        index = new_array.length-1;
+        }else{
+        index--;
+        }
+     div.remove()
+     display_data(new_array[index])
+
+    }
+
+  }
+  function touchMove(event){
+    if(isDragging){
+      const currentPosition = getPositionX(event)
+      currentTranslate = prevTranslate + currentPosition - startPos
+
+    }
+}
+function getPositionX(event) {
+  return event.type.includes('mouse') ? event.pageX : event.touches[0].clientX
+}
 
 
